131e452312fc0d0e45526e4a3c4338892863aa66
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-09-05 15:29:07 -0300

    Adicionado estruturas HTML e CSS.

---
+++ script.js
@@ -0,0 +1,8 @@
+var nome = " Guilherme"
+var notaDoPrimeiroBimestre = 9.5898
+var notaDoSegundoBimentre = 7.987
+var notaDoTerceiroBimente = 4.478
+var notaDoQuartoBimestre =2.6585
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimentre + notaDoTerceiroBimente + notaDoQuartoBimestre) / 4;
+var notafixada = notaFinal.toFixed(1)
+console.log (notafixada)
